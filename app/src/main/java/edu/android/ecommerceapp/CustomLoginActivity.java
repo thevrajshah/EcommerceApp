2e6ea18c5e4d1853e6bdd6bf0050c6e3f8350e8b
imjp19 <jaypatel19124@gmail.com>
2021-04-08 14:22:23 +0530

    CustomLoginActivity java & design

--- app/src/main/java/edu/android/ecommerceapp/CustomLoginActivity.java
+++ app/src/main/java/edu/android/ecommerceapp/CustomLoginActivity.java
@@ -2,20 +2,42 @@ package edu.android.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class CustomLoginActivity extends AppCompatActivity {
-
     private static final String TAG = "CustomLoginActivity";
+
+
+    EditText email;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_login);
+
+        email = findViewById(R.id.usernameCustom);
+        password = findViewById(R.id.passwordCustom);
     }
 
-    public void xyz(View view) {
-        Log.d(TAG, "xyz: clicked on button");
+    public void login(View view) {
+
+        if(email.getText().toString().matches(".*@.*..*") && email.getText().toString().equals("svitcms@svit.ac.in")){
+            if (password.getText().toString().equals("svitcms")) {
+                Intent i = new Intent(CustomLoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+            else {
+                password.setError("Alya badhu sarkhu nakh!!");
+            }
+        }
+        else {
+            email.setError("Alya badhu sarkhu nakh!!");
+        }
     }
 }
